a361991359d0c237fde1de25afab29f9993f5990
benbasse <moussabasse221@gmail.com>
2023-11-02 12:32:39 +0000

    Pour controller les contacts à enregistrer

--- controller/contact.php
+++ controller/contact.php
@@ -1,24 +1,25 @@
 <?php
 include_once("../model/classContact.php");
 include_once("../model/classdb.php");
+ini_set('display_errors', 'On');
 
-class contactController {
-    public function insertContact() {
-        if (!empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["numero_telephone"]) && isset($_POST['favori'])) {
+class contactController 
+{
+    public function insertContact() 
+    {
+        if (!empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["numero_telephone"]) && isset($_POST['favori'])) 
+        {
             $nom = $_POST['nom'];
             $prenom = $_POST['prenom'];
             $numero_telephone = $_POST['numero_telephone'];
             $favori = $_POST['favori'];
-            $userContact = new Contact($nom, $prenom, $numero_telephone, $favori);
-            
-            if ($userContact->insertContact()) {
-                echo "Insertion réussie";
-                die();
-            } else {
-                echo "L'enregistrement n'a pas réussi";
-            }
+            // $id = $_GET['$id'];
+            $userContact = new Contact();
+            $userContact->insertContact($nom, $prenom, $numero_telephone, $favori);
+            header("location: ../view/listeContact.php");
         }
     }
+
 }
 
 $Usercontact = new contactController();
