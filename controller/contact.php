e35c7ff1a0035d53dade3f60b163fbc70a34c81c
benbasse <moussabasse221@gmail.com>
2023-11-03 11:32:18 +0000

    le controller qui nous permet de controller les contacts enregistrer des contacts

--- controller/contact.php
+++ controller/contact.php
@@ -7,15 +7,44 @@ class contactController
 {
     public function insertContact() 
     {
+        if (empty($_POST["nom"]) && empty($_POST["prenom"]) && empty($_POST["numero_telephone"]))
+        {
+            echo "<p style = color:red>All fields are mandatory</p>";
+        } 
+
         if (!empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["numero_telephone"]) && isset($_POST['favori'])) 
         {
-            $nom = $_POST['nom'];
-            $prenom = $_POST['prenom'];
-            $numero_telephone = $_POST['numero_telephone'];
-            $favori = $_POST['favori'];
-            $userContact = new Contact();
-            $userContact->insertContact($nom, $prenom, $numero_telephone, $favori);
-            header("location: ../view/listeContact.php");
+            // Les expresions réguliers
+            $nom = htmlspecialchars($_POST['nom']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$nom)) 
+            {
+                echo "<p style = color:red>Only letters and white space allowed</p>";
+                return;
+            } 
+
+            $prenom = htmlspecialchars($_POST['prenom']);
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$prenom)) 
+            {
+                echo "<p style = color:red>Only letters and white space allowed</p>";
+                return;
+            } 
+
+            $numero_telephone = htmlspecialchars($_POST['numero_telephone']);
+            if (strlen($numero_telephone) < 9)
+            {
+                echo '<p style = color:red>Your number is incorrect</p>';
+                return;
+            } 
+
+            if ($_POST['favori'] == 'oui' || $_POST['favori'] == 'non') 
+            {
+                $favori = $_POST['favori'];
+            }
+
+                $userContact = new Contact();
+                $userContact->insertContact($nom, $prenom, $numero_telephone, $favori);
+                header("location: ../view/listeContact.php");
+            
         }
     }
 
@@ -23,4 +52,3 @@ class contactController
 
 $Usercontact = new contactController();
 $Usercontact->insertContact();
-?>
